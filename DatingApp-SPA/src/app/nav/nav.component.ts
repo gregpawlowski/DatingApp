87331c8147a91a1643dbfc94d046b0f38cb6008c
Greg Pawlowski <gpawlohf@walgreenco.net>
2020-01-09 16:17:48 -0600

    Added angular register and auth services

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit, AfterViewInit {
+  model: any = {};
+
+  @ViewChild('loginForm', { static: false }) loginForm: NgForm;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    console.log(this.loginForm);
+  }
+
+  ngAfterViewInit() {
+    console.log('after view', this.loginForm);
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.model)
+        .subscribe((next) => {
+          console.log('Logged in successfully');
+        }, (error) => {
+          console.log('Failed to log in');
+        });
+    }
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
+
+}
